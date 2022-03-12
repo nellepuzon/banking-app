debde625ab2ea2037f9b506a8f7ba7628201e0d2
Junelle Puzon <nellepuzon@gmail.com>
2022-03-12 14:13:23 +0800

    added admin page

---
+++ src/components/AdminPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from './admin-page/NavBar'
+import Table from './admin-page/Table'
+
+function AdminPage() {
+    return (
+        <div class="admin-page">
+            <NavBar/>
+            <Table/>
+        </div>
+    )
+}
+
+export default AdminPage;
---
+++ src/components/admin-page/Table.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+
+function Table() {
+  return (
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Balance</th>
+      </tr>
+      <tr>
+        <td>
+          <a class='eg' href='#'>
+            Malunes, Eglecerio
+          </a>
+        </td>
+        <td>
+          <i class='fa-solid fa-peso-sign'></i> 11,297
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <a href='#'>Puzon, Junelle</a>
+        </td>
+        <td>
+          <i class='fa-solid fa-peso-sign'></i> 53,928
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <a href='#'>Go, Francis</a>
+        </td>
+        <td>
+          <i class='fa-solid fa-peso-sign'></i> 53,928
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <a href='#'>Turingan, Joshua</a>
+        </td>
+        <td>
+          <i class='fa-solid fa-peso-sign'></i> 90,287
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <a href='#'>Reza Jhay Lacanlale</a>
+        </td>
+        <td>
+          <i class='fa-solid fa-peso-sign'></i> 34,387
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <a href='#'>Alphonzo Escolar</a>
+        </td>
+        <td>
+          <i class='fa-solid fa-peso-sign'></i> 23,989
+        </td>
+      </tr>
+      <tr>
+        <td>
+          <a href='#'>Spike Vinz Cruz</a>
+        </td>
+        <td>
+          <i class='fa-solid fa-peso-sign'></i> 43,237
+        </td>
+      </tr>
+    </table>
+  );
+}
+
+export default Table;
