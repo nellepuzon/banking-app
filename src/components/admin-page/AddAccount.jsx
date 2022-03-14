5c14c8e21b4d0e402737b43a5fcf0ec0881b4aff
eglecerio <egleceriom425@gmail.com>
2022-03-14 12:39:10 +0800

    add account error handle

--- src/components/admin-page/AddAccount.jsx
+++ src/components/admin-page/AddAccount.jsx
@@ -7,6 +7,7 @@ function AddAccount({setAccounts, accounts}) {
     const [balance, setBalance] = useState('')
     const [userName, setUserName] = useState('')
     const [password, setPassword] = useState('')
+    const [errorMessage, setErrorMessage] = useState('')
 
     const handleFullName = (e) => {
         setFullName(e.target.value)
@@ -26,17 +27,33 @@ function AddAccount({setAccounts, accounts}) {
 
     const handleAddAccount = (e) => {
         e.preventDefault();
+        const userNameMatch = accounts.find(element => element.userName === userName)
+        const fullNameMatch = accounts.find(element => element.fullName === fullName)
 
-        if (fullName !== '' && balance !== '') {
+        if (userNameMatch && !fullNameMatch && fullName !== '' && userName !== '' && password !== '' && balance !== '') {
+            setErrorMessage({message:"username already exists"})
+        } else if (fullNameMatch && !userNameMatch && fullName !== '' && userName !== '' && password !== '' && balance !== '') {
+            setErrorMessage({message:"full name already exists"})
+        } else if (userNameMatch && fullNameMatch && fullName !== '' && userName !== '' && password !== '' && balance !== '') {
+            setErrorMessage({message:"account already exists"})
+        } else if (fullName !== '' && balance !== '' && !userNameMatch && !fullNameMatch) {
             setAccounts([...accounts, { userName: userName, password: password, type: 'user', fullName: fullName, money: balance }])
             setFullName('');
             setBalance('');
             setUserName('');
             setPassword('');
+        } else {
+            setErrorMessage({message:"fill out necessary information"})
         }
     }
+    const renderError = () => {
+        if(errorMessage) {
+            return <div className='error-message'>{errorMessage.message}</div>
+          }
+    } 
     return (
         <div className="add-account-container">
+            {renderError()}
             <div className="add-account-inputs">
                 <input className="input-username" type="text" placeholder="Username" value={userName} onChange={handleUsername}></input>
                 <input className="input-password" type="password" placeholder="Password" value={password} onChange={handlePassword}></input>
