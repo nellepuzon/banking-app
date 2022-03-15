ddf8796d9db8d01b5f7d74c670a31e27aab0f704
eglecerio <egleceriom425@gmail.com>
2022-03-16 01:31:36 +0800

    edit account feature

--- src/components/AdminPage.jsx
+++ src/components/AdminPage.jsx
@@ -8,14 +8,42 @@ import Deposit from "./transaction-page/Deposit";
 import Withdraw from "./transaction-page/Withdraw";
 
 function AdminPage(props) {
+    const [fullName, setFullName] = useState('')
+    const [balance, setBalance] = useState('')
+    const [userName, setUserName] = useState('')
+    const [password, setPassword] = useState('')
     const [searchInput, setSearchInput] = useState("")
+    const [isEditing, setIsEditing] = useState(false)
+    const [editingID, setEditingID] = useState(0)
     return (
         <div className="admin-page">
             <NavBar searchInput={searchInput} setSearchInput={setSearchInput} setIsAdmin={props.setIsAdmin} setLoggedIn={props.setLoggedIn}/>
-            <Table accounts={props.accounts} searchInput={searchInput} setAccounts={props.setAccounts}/>
+            <Table 
+            accounts={props.accounts} 
+            searchInput={searchInput} 
+            setAccounts={props.setAccounts}
+            setFullName={setFullName}
+            setBalance={setBalance}
+            setUserName={setUserName}
+            setPassword={setPassword}
+            setEditingID={setEditingID}
+            setIsEditing={setIsEditing}
+            />
             <AddAccount 
             setAccounts={props.setAccounts}
-            accounts={props.accounts}/>
+            accounts={props.accounts}
+            fullName={fullName}
+            setFullName={setFullName}
+            balance={balance}
+            setBalance={setBalance}
+            userName={userName}
+            setUserName={setUserName}
+            password={password}
+            setPassword={setPassword}
+            isEditing={isEditing}
+            setIsEditing={setIsEditing}
+            editingID={editingID}
+            />
             <Transactions/>
             <Deposit accounts={props.accounts} setAccounts={props.setAccounts}/>
             <Withdraw accounts={props.accounts} setAccounts={props.setAccounts}/>
--- src/components/admin-page/TableRow.jsx
+++ src/components/admin-page/TableRow.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 
 
-function TableRow({account, setAccounts, accounts}) {
+function TableRow({account, setAccounts, accounts, fullName, setFullName, balance, setBalance, userName, setUserName, password, setPassword, setEditingID, setIsEditing}) {
+
   const handleDelete = (id) => {
     const newUsers = accounts.filter((user) => user.accountNumber !== id)
     console.log(newUsers)
     setAccounts(newUsers)
     localStorage.setItem("accounts", JSON.stringify([...newUsers]))
   }
+
+  const handleEdit = (id) => {
+    setEditingID(id)
+    setIsEditing(true)
+    const selectedUser = accounts.find((user) => user.accountNumber === id)
+    setFullName(selectedUser.fullName)
+    setBalance(selectedUser.money)
+    setUserName(selectedUser.userName)
+    setPassword(selectedUser.password)
+  }
+
     return (
     <tr>
       <td>
@@ -20,7 +32,7 @@ function TableRow({account, setAccounts, accounts}) {
           <i className='fa-solid fa-peso-sign'></i> {account.money}
         </td>
         <div className='edit-delete'>
-        <i className="fa-solid fa-pen-to-square edit-button"></i>
+        <i className="fa-solid fa-pen-to-square edit-button" onClick={() =>{handleEdit(account.accountNumber)}}></i>
         <i className="fa-solid fa-trash-can delete-button" onClick={() =>{handleDelete(account.accountNumber)}}></i></div>
     </tr>
     )
