08da8c6c206488a242a74a07227311c02a055efc
eglecerio <egleceriom425@gmail.com>
2022-03-22 16:35:34 +0800

    react-router initial route improvement

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "../src/styles/output.css";
 import LoginPage from "./components/LoginPage";
 import AdminPage from "./components/AdminPage";
 import UserPage from "./components/UserPage";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import LoginCheck from "./components/login-page/LoginCheck";
 import { useContext } from "react";
 import DataContext from "./context/DataContext";
@@ -10,11 +10,12 @@ import DataContext from "./context/DataContext";
 function App() {
   return (
     <Routes>
-      <Route path="/*" element={<LoginPage />} />
+      <Route path="/login" element={<LoginPage />} />
       <Route element={<LoginCheck />}>
         <Route path="/admin" element={<AdminPage />} />
         <Route path="/user/:userInput" element={<UserPage />} />
       </Route>
+      <Route path="*" element={<Navigate to="/login" replace />} />
     </Routes>
   );
 }
