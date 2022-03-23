08355b9df7465bcbeea039e5be35d4c2739ed4ca
Junelle Puzon <nellepuzon@gmail.com>
2022-03-23 09:17:03 +0800

    media queries

--- src/components/admin-page/Search.jsx
+++ src/components/admin-page/Search.jsx
@@ -1,12 +1,20 @@
 import React, { useContext } from "react";
 import AdminContext from "../../context/AdminDataContext";
+import DataContext from "../../context/DataContext";
 
 function Search() {
+  const { adminLogin } = useContext(DataContext);
     const { onSearch } = useContext(AdminContext);
 
   return (
     <div className='search-bar'>
-        Accounts
+        <div className="accounts-title">Accounts         <i
+              className='fa-solid fa-right-from-bracket dashboard-icon logout-mobile'
+              onClick={() => {
+                adminLogin(false);
+              }}
+            ></i>
+</div>
       <input
         className='search'
         type='text'
